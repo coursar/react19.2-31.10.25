94312bd86b10dbf02265a502f3845d2036831aa1
student <student@local.dev>
2025-10-31 20:39:30 +0300

    day 01

---
+++ src/App/App.jsx
@@ -0,0 +1,7 @@
+// 19.2 -> 1 october
+const App = () => {
+  // components
+  return <div>Some Content</div>
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+import { createElement, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App/App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const el = <div></div> // createElement(App, ...)
+// stopped here
+
+// HTML "Analogue" => rootEl.innerHTML = <App />
+// createRoot(document.getElementById('root')).render(
+//   <App />
+// )
