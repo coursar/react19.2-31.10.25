1cb04435b3559f31cbf8554688dd36262177cabb
student <student@local.dev>
2025-11-01 18:51:22 +0300

    useEffect

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,13 +2,26 @@
 //  - JSX -> React.Element
 //  - string, ..., undefined
 
-// TODO: top mistakes
+import { useEffect } from "react"
+
+// TODO: top mistakes for return
 //  1. omit return
 //  2. multiple JSX elements
 //  3. return + EOL => return; => return undefined;
 // SOLUTION => return (<> ... </>)
 const App = () => {
   // React.Fragment -> container, doesn't create any DOM
+
+  // TODO: top mistakes inside component
+  // 1. write function to get data (Suspense + Data Fetching)
+
+  // Hooks -> use+, (API - not hook use())
+  // Lifecycle:
+  // 1. Mount -> creation + adding "to page"
+  useEffect(() => {
+    console.log('after mount')
+  }, []) // [] - executes "on mount" (after mount) - dependencies
+
   return (
     <>
       Messages
